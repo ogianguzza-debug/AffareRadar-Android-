0edf2c24162782b661708d68c1dd8d434d7544f9
ogianguzza-debug <ogianguzza@gmail.com>
2026-09-10 16:05:30 +0200

    Add boot receiver

---
+++ app/src/main/java/it/affareradar/worker/BootReceiver.java
@@ -0,0 +1,13 @@
+package it.affareradar.worker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            // NotificationListenerService is managed by Android after user permission is granted.
+        }
+    }
+}
